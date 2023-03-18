34c1c60079514175f5dc03f25de2061f02e2b6db
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2023-03-19 00:06:06 +0100

    favicon URL corrected

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <link rel="icon" href="/logo.png" />
+      <link rel="icon" href="/logo.svg" />
       <Head />
       <body>
         <Main />
