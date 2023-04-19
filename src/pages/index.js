5acf36283516e05bba1b1a216c274e2f9260cb4f
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2023-04-19 21:14:49 +0200

    added autocomplete search 🎉

---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Header from '../../components/Header/header'
+import Turnstone from 'turnstone'
+import { useRouter } from 'next/router'
+const inter = Inter({ subsets: ['latin'] })
+export default function Home() {
+  const router = useRouter();
+  function searchstation(){
+    const getsearchvalue = document.getElementById("searchvalue").value
+    const getnumberofresults = document.getElementById("numberofresults").value
+    if (getsearchvalue != ""){
+      const redirecturl = "/departures/" + getsearchvalue + "&results=" + getnumberofresults
+      router.push(redirecturl)
+      }
+      if (getsearchvalue ==""){
+        router.push("/departures")
+      }
+  }
+  const listbox  = {
+    displayField: 'name',
+    data: (query) =>
+      fetch(`https://transport.phipsiart.de/locations?poi=false&addresses=false&query=${query}&results=5`)
+        .then(response => response.json()),
+    searchType: 'startsWith', 
+  }
+  const searchstyles= {
+    input:'searchinput',
+    typeahead: 'grey',
+    listbox:'searchresults',
+  }
+
+
+
+  return (
+    <>
+      <Head>
+        <title>Bahnhofsabfahrten</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Header></Header>
+      <main className={inter.className}>
+        <h1 className={styles.headline}>Bahnhof ausw&auml;hlen</h1>
+        <div className={styles.search} id={styles.searchfield}>
+        <Turnstone  id='searchvalue'
+      name='search'
+      styles={searchstyles}
+      autoFocus={true}
+      typeahead={true}
+      debounceWait={250}
+      listboxIsImmutable={true}
+      maxItems={5}
+      listbox={listbox}
+      noItemsMessage="Bahnhof nicht gefunden"
+      placeholder='Bahnhof suchen'
+    /> 
+      <input  autoComplete="off" role="presentation" required className={styles.boxanimation2} type="number" id="numberofresults" placeholder="Anzahl der Ergebnisse" defaultValue="10"></input>
+       <button onClick={searchstation} className={styles.inputbutton}>Suchen</button>
+       </div>
+       <div id='footer'>
+        <p className={styles.footertext}>Alle Angaben ohne Gew&auml;hr.</p>
+       </div>
+      </main>
+    </>
+  )
+}
